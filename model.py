c866fdaeeef41286426d3b08f583a7bef3457451
Charles Lu <charleslu@live.com>
2018-05-23 21:05:13 +0000

    checkin old work

--- model.py
+++ model.py
@@ -3,20 +3,40 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Flatten3d(nn.Module):
+class Flatten(nn.Module):
     def forward(self, x):
-        N, C, D, H, W = x.size()
+        N, C, H, W = x.size()
         return x.view(N, -1)
 
 class OctNet(nn.Module):
     def __init__(self):
         super(OctNet, self).__init__()
+        
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3),
+            nn.BatchNorm2d(num_features=16),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(16, 32, kernel_size=3),
+            nn.BatchNorm2d(num_features=32),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(32, 16, kernel_size=3),
+            nn.BatchNorm2d(num_features=16),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            Flatten(),
+            nn.Linear(46368, 1)
+        )
+        self.optimizer = torch.optim.Adam(self.parameters())
+        self.loss_fn = nn.BCEWithLogitsLoss()
     
-    def forward(self, cubes):
-        return self.cnn(cubes).squeeze()
+    def forward(self, slice_):
+        # print(self.cnn(slice_).size())
+        return self.cnn(slice_).squeeze()
     
-    def train_step(self, cubes, targets):
-        logits = self(cubes)
+    def train_step(self, slice_, targets):
+        logits = self(slice_)
         loss = self.loss_fn(logits, targets)
         
         self.optimizer.zero_grad()
@@ -24,22 +44,3 @@ class OctNet(nn.Module):
         self.optimizer.step()
         
         return loss
-
-class Oct200Net(OctNet):
-    def __init__(self):
-        super(Oct200Net, self).__init__()
-        
-        self.cnn = nn.Sequential(
-            nn.Conv3d(1, 16, kernel_size=8),
-            nn.BatchNorm3d(num_features=16),
-            nn.LeakyReLU(),
-            nn.MaxPool3d(kernel_size=4),
-            nn.Conv3d(16, 16, kernel_size=4),
-            nn.BatchNorm3d(num_features=16),
-            nn.LeakyReLU(),
-            nn.MaxPool3d(kernel_size=4),
-            Flatten3d(),
-            nn.Linear(224, 1)
-        )
-        self.optimizer = torch.optim.Adam(self.parameters())
-        self.loss_fn = nn.BCEWithLogitsLoss()
