099fad49998c7756fe4607491864df7bd0c50596
Charles Lu <charleslu@live.com>
2018-08-10 03:36:37 +0000

    add new referral/nonreferral 5/6 data

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@ PROCESSED_DATA_PATH = '../oct-data'
 CUBES_FOLDER = 'cubes'
 MODELS_PATH = '../models'
 
-NEG_DIRS = ['Non referral1', 'Non Referral2', 'NonReferral3', 'NonReferral4']
-POS_DIRS = ['Referral1', 'Referral2', 'Referral3', 'Referral4']
+NEG_DIRS = ['Non referral1', 'Non Referral2', 'NonReferral3', 'NonReferral4', 'NonReferral5', 'NonReferral6']
+POS_DIRS = ['Referral1', 'Referral2', 'Referral3', 'Referral4', 'Referral 5', 'Referral6']
 
 anonymize_mapping_file = 'mapping.csv'
 
